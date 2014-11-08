24d818a105d737dcad100a5077843846d57e83ce
Joseph Leon <joseph@poeticsystems.com>
2014-11-08 22:24:45 +0200

    Make admin interface changes for Question and Choice.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                {'fields': ['question_text']}),
+        ('Date information',  {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
