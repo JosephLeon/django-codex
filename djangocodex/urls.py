b54cc290c9323ca3cd916e63745f6675eea5d182
Joseph Leon <joseph@poeticsystems.com>
2014-11-09 00:16:18 +0200

    Add url pattern for polls.

--- djangocodex/urls.py
+++ djangocodex/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djangocodex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
