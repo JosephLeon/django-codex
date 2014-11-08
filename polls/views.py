0a7dd637096c0b7860072964f5dec92ea67aa879
Joseph Leon <joseph@poeticsystems.com>
2014-11-09 01:28:00 +0200

    Update polls index view to list Question objects.

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("You're at the polls index.")
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_questions_list])
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at questions %s." % question_id)
 
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
