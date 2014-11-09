341a1229f74cc356f040bb719f84d5d6771ebd15
Joseph Leon <joseph@poeticsystems.com>
2014-11-09 02:53:14 +0200

    Add question index template and rewrite the index url.

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from polls.models import Question
 
 
 def index(request):
-    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_questions_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
 
 def detail(request, question_id):
