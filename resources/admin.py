be190501dc08f08ec463ab6b92426073c0f66b3a
Joseph Leon <joseph@poeticsystems.com>
2014-11-10 20:49:53 +0200

    Add resource review to the resource object in the admin panel.

--- resources/admin.py
+++ resources/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from resources.models import Resource, UserReview
 
-admin.site.register(Resource)
-admin.site.register(UserReview)
+
+class UserReviewInline(admin.StackedInline):
+    model = UserReview
+    extra = 1
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    inlines = [UserReviewInline]
+
+admin.site.register(Resource, ResourceAdmin)
