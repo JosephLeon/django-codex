799b996deeda3052d1975d6747cfc239b3b6ed99
Joseph Leon <joseph@poeticsystems.com>
2014-11-10 16:49:30 +0200

    Add 404 logic to details def and create a details template.

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponse, Http404
 from polls.models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at questions %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
