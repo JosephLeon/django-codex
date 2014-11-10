06315df62baa38733a1507db514da6fc6013240a
Joseph Leon <joseph@poeticsystems.com>
2014-11-10 19:57:39 +0200

    Generate new app called resource, create models for resource app.

---
+++ resources/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Resource(models.Model):
+    resource_title = models.Charfield()
+    pub_date = models.DateTimeField('date published')
+    resource_link = models.URLField()
+    resource_review = models.TextField()
+
+
+class UserReview(models.Model):
+    resource = models.ForeignKey(Resource)
+    user_review = models.TextField()
+    votes = models.IntegerField(default=0)
