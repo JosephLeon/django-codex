d0ff97633ec19dac2328ccb31f8b99212e460edb
Joseph Leon <joseph@poeticsystems.com>
2014-11-11 15:08:07 +0200

    Update DB with sample data, change admin title, add list display to admin.

--- resources/admin.py
+++ resources/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from resources.models import Resource, UserReview
 
 
-class UserReviewInline(admin.StackedInline):
+class UserReviewInline(admin.TabularInline):
     model = UserReview
     extra = 1
 
 
 class ResourceAdmin(admin.ModelAdmin):
     inlines = [UserReviewInline]
+    list_display = ('resource_title', 'resource_link', 'pub_date')
 
 admin.site.register(Resource, ResourceAdmin)
